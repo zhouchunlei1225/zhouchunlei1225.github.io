e5cbca7c41b38d782bb208c2634367359fef42b2
zhouchunlei <2388184360@qq.com>
2022-10-20 13:29:31 +0800

    Site updated: 2022-10-20 13:29:27

---
+++ js/extend.js
@@ -0,0 +1,74 @@
+//  拓展的js
+
+// 禁止右键和F12
+function forbidden_control() {
+    $.extend({
+        message: function(a) {
+            var b = {
+                title: "提示",
+                message: "操作成功",
+                time: "3000",
+                type: "success",
+                showClose: !0,
+                autoClose: !0,
+                onClose: function() {}
+            };
+            "string" == typeof a && (b.message = a), "object" == typeof a && (b = $.extend({}, b, a));
+            var c, d, e, f = b.showClose ? '<div class="c-message--close">×</div>' : "",
+                g = "" !== b.title ? '<h2 class="c-message__title">' + b.title + "</h2>" : "",
+                h = '<div class="c-message animated animated-lento slideInRight"><i class=" c-message--icon c-message--' + b.type + '"></i><div class="el-notification__group">' + g + '<div class="el-notification__content">' + b.message + "</div>" + f + "</div></div>",
+                i = $("body"),
+                j = $(h);
+            d = function() {
+                j.addClass("slideOutRight"), j.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function() {
+                    e()
+                })
+            }, e = function() {
+                j.remove(), b.onClose(b), clearTimeout(c)
+            }, $(".c-message").remove(), i.append(j), j.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function() {
+                j.removeClass("messageFadeInDown")
+            }), i.on("click", ".c-message--close", function(a) {
+                d()
+            }), b.autoClose && (c = setTimeout(function() {
+                d()
+            }, b.time))
+        }
+    }),
+        document.onkeydown = function(e) {
+            if (123 == e.keyCode || e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode) || (e.ctrlKey && 85 === e.keyCode)) return $.message({
+                message: "采用本站js及css请注明来源，禁止商业使用！",
+                title: "你真坏，不能打开控制台喔!",
+                type: "error",
+                autoHide: !1,
+                time: "3000"
+            }), event.keyCode = 0, event.returnValue = !1, !1
+        }, document.oncontextmenu = function() {
+        return $.message({
+            message: "采用本站js及css请注明来源，禁止商业使用！",
+            title: "不能右键/长按喔！",
+            type: "error",
+            autoHide: !1,
+            time: "3000"
+        }), !1
+    }
+}
+forbidden_control()
+
+
+// 动态小人
+function initSakanaWidget() {
+    if (!/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+
+        const ikun = SakanaWidget.getCharacter('chisato');
+        ikun.image = './img/kun.png';
+        SakanaWidget.registerCharacter('ikun', ikun,);
+
+        new SakanaWidget({
+            // character: 'ikun' | 'chisato' | 'takina', // 角色
+            controls: true, // 是否显示控制台
+            // size : 400, // 组件大小
+            // autoFit: true, //自动适应容器大小
+        }).setState().mount('#sakana-widget');
+    }
+
+}
